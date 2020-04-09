2c7b62f544222ddebea1dc8c2f246388f3f4ee92
taty2010 <shydalys123@gmail.com>
2020-04-09 11:52:34 -0500

    updates to server and chat

--- server/index.js
+++ server/index.js
@@ -1,16 +1,21 @@
 const express = require('express');
 const socketio = require('socket.io');
 const http = require('http');
+const cors = require('cors');
+require('dotenv').config();
 
 const { addUser, removeUser, getUser, getUsersInRoom } = require('./users')
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 
 const router = require('./router');
 
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
+
+app.use(cors());
+app.use(router);
 
 io.on('connect', (socket) => {
   // console.log('We have a new connection!!!');
@@ -49,9 +54,7 @@ io.on('connect', (socket) => {
   })
 });
 
+server.listen(PORT, () =>{ 
+console.log(`server has started on port ${PORT}`)
 
-app.use(router);
-
-server.listen(PORT, () => console.log(
-  `server has started on port ${PORT}`
-));
+});
